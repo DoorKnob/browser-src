bd90807c6f243773efecb312e1243413f3b795de
DoorKnob <jojobeni1@gmail.com>
2013-05-14 18:50:19 -0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,11 @@
+package leBrowser;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Cat {
+  public static void main(String[] args) {
+		Bacon dude = new Bacon();
+		dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
